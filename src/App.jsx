b95c094a058589152e4c6d27860d99cb1e6a29fd
sohseyedi <sohseyedi@gmail.com>
2023-02-26 14:10:09 +0330

    create base

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+ 
+
+  return (
+    <div className="text-center">
+      <div className="text-center text-xl">
+        soheil
+      </div>
+    </div>
+  )
+}
+
+export default App
