a7ad937debf94550805ee73efab8bc391ba0a1c7
sohseyedi <sohseyedi@gmail.com>
2023-02-26 17:18:07 +0330

    complete backend

--- api/index.js
+++ api/index.js
@@ -9,10 +9,32 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/" , (req , res) => {
-    res.status(200).send({message : "this is chatgpt message"})
-})
+app.get("/", (req, res) => {
+  res.status(200).send({ message: "this is chatgpt message" });
+});
 
-app.listen(4000 , () => {
-    console.log("server is run");
-})
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+app.post("/", async (req, res) => {
+  try {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: req.body.input,
+      temperature: 0,
+      max_tokens: 4000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    });
+    res.status(200).send({ bot: response.data.choices[0].text });
+  } catch (error) {
+    console.log(err.message);
+  }
+});
+
+app.listen(4000, () => {
+  console.log("server is run");
+});
